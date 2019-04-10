f21ebf681a5ccfa1798b50cea447ae6a7849e1d4
Merijn Verstraaten <merijn@inconsistent.nl>
2019-04-10 14:17:12 +0200

    Remove trailing whitespace

--- src/fft.hh
+++ src/fft.hh
@@ -10,4 +10,3 @@ extern void fft_test(std::string const &filename, unsigned fft_size, unsigned bl
 template <typename T>
 using complex_span = gsl::span<std::complex<T>>;
 using shape_t = std::vector<int>;
-
