f118e8eb50f0591781eb3a442ce4ad9a11a74ae6
Merijn Verstraaten <merijn@inconsistent.nl>
2019-08-15 10:28:36 +0200

    Emulate with strict channel depths

--- Rules.mk
+++ Rules.mk
@@ -1,10 +1,12 @@
+.PRECIOUS: $(DEVICE_KERNEL_DIR)/%.aoco
+
 $(BUILD_DIR)/%.o: $(SRCDIR)/%.cc | $(BUILD_DIR)/
 	$(PRINTF) " CC\t$(notdir $<)\n"
 	$(AT)$(CXX) $(CXXFLAGS) -MMD -MP -c $< -o $@
 
 $(EMULATOR_KERNEL_DIR)/%.aoco: $(SRCDIR)/%.cl | $(EMULATOR_KERNEL_DIR)
 	$(PRINTF) " AOC\t$(^F)\n"
-	$(AT)$(AOC) $(AOCFLAGS) -c -march=emulator -board=p385a_min_ax115 \
+	$(AT)$(AOC) $(AOCFLAGS) -c -march=emulator -emulator-channel-depth-model=strict -board=p385a_min_ax115 \
 	    -o $@ $^ $(if $(AT),2>/dev/null >/dev/null,)
 
 $(DEVICE_KERNEL_DIR)/%.aoco: $(SRCDIR)/%.cl | $(DEVICE_KERNEL_DIR)
@@ -19,7 +21,7 @@ $(DEVICE_KERNEL_DIR)/%.aocr: $(DEVICE_KERNEL_DIR)/%.aoco
 
 $(EMULATOR_KERNEL_DIR)/%.aocr: $(EMULATOR_KERNEL_DIR)/%.aoco
 	$(PRINTF) " AOC\t$(^F)\n"
-	$(AT)$(AOC) $(AOCFLAGS) -march=emulator -board=p385a_min_ax115 $^ -rtl \
+	$(AT)$(AOC) $(AOCFLAGS) -march=emulator -emulator-channel-depth-model=strict -board=p385a_min_ax115 $^ -rtl \
 	    -o $@ $(if $(AT),2>/dev/null >/dev/null,)
 
 $(DEVICE_KERNEL_DIR)/%.aocx: $(DEVICE_KERNEL_DIR)/%.aocr
