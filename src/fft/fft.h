5c4119c8e94ce89cdeca7b0579ca033447b6666f
Ben van Werkhoven <b.vanwerkhoven@esciencecenter.nl>
2020-01-25 16:22:48 +0100

    first working on emulator 1024 point FFT C2C

---
+++ src/fft/fft.h
@@ -0,0 +1,14 @@
+
+#define R float
+#define E R
+#define stride int
+#define INT int
+#define K(x) ((E) x)
+#define DK(name,value) const E name = K(value)
+#define WS(s,i) s*i
+#define MAKE_VOLATILE_STRIDE(x,y) 0
+#define FMA(a,b,c) a * b + c
+#define FMS(a,b,c) a * b - c
+#define FNMA(a,b,c) -a * b - c
+#define FNMS(a,b,c) -a * b + c
+
